d5ad0c05163c3dbbe856110a52584e7ad952ddc6
XDean <373216024@qq.com>
2018-04-04 14:58:58 +0800

    init

---
+++ server/src/main/java/xdean/spring/cloud/learning/CloudServerApp.java
@@ -0,0 +1,12 @@
+package xdean.spring.cloud.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerApp.class, args);
+	}
+}
