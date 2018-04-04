350d4cda462e7a01a19f2b5ab1a9b926a50d273c
XDean <373216024@qq.com>
2018-04-04 17:14:14 +0800

    init server and client

--- client/src/main/java/xdean/spring/cloud/learning/CloudClientApp.java
+++ client/src/main/java/xdean/spring/cloud/learning/CloudClientApp.java
@@ -2,11 +2,21 @@ package xdean.spring.cloud.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class CloudClientApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudClientApp.class, args);
 	}
+	
+  @RequestMapping("/")
+  public String home() {
+      return "Hello world";
+  }
 }
